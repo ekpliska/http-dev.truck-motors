074f58555c31363e10cf7c8dca221d20529bb768
ekpliska <pliska.ek@gmail.com>
2018-06-26 08:48:57 +0300

    Test push

--- views/site/service-rec.php
+++ views/site/service-rec.php
@@ -3,6 +3,7 @@
     use yii\widgets\Breadcrumbs;
     use app\widgets\Info;
     use app\widgets\MainServices;
+    use app\widgets\SubMenu;
 /* 
  * Запись на СТО
  */
@@ -17,29 +18,33 @@ $this->params['breadcrumbs'][] = 'Запись на сервис';
 ?>
 <section class="site-service">
     <div class="container">
-        <h3>Запись на сервис</h3>
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ])
-        ?>
-             
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <div class="col-sm-6 text-center">
-                    <?=
-                        Html::a('Физическое лицо', ['site/record-ind'], ['class' => 'btn btn-sm animated-button ind']);
-                    ?>
-                </div>
-                <div class="col-sm-6 text-center">
-                    <?=
-                        Html::a('Юридическое лицо', ['site/record-leg'], ['class' => 'btn btn-sm animated-button leg']);
-                    ?>
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+            <h3>Запись на сервис</h3>
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                ])
+            ?>
+
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="col-sm-6 text-center">
+                        <?=
+                            Html::a('Физическое лицо', ['site/record-ind'], ['class' => 'btn btn-sm animated-button ind']);
+                        ?>
+                    </div>
+                    <div class="col-sm-6 text-center">
+                        <?=
+                            Html::a('Юридическое лицо', ['site/record-leg'], ['class' => 'btn btn-sm animated-button leg']);
+                        ?>
+                    </div>
                 </div>
             </div>
+
+            <?= Info::widget(['alias_block' => 'schedule_block']) ?>
         </div>
-        
-        <?= Info::widget(['alias_block' => 'schedule_block']) ?> 
-        
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            <?= SubMenu::widget(); ?>
+        </div>        
         <?= MainServices::widget(['view_name' => 'mainservices']); ?>
     </div>                
 </section>
